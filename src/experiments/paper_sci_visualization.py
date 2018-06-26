8193b8adabc93eb2a4e440fd2003a8ed8bb3b08b
unknown <clean1009@126.com>
2018-06-26 12:52:06 +0800

    add fails statistics

--- src/experiments/paper_sci_visualization.py
+++ src/experiments/paper_sci_visualization.py
@@ -1,38 +1,61 @@
 import numpy as np
 import datahandler.wsdream_handler as wh
 from matplotlib import pyplot as plt
+import json
 
 def load_result():
     '''
     加载不同ratio下的mae矩阵（#hash_table * #hash_function)
     :return:一个#ratio * #hash_table * #hash_function的rmae和fail
     '''
-    ratios = [2, 5, 7, 9]
     rmaes = []
+    average_rames = []
     fails = []
 
-    for ratio in ratios:
-        rmae_file_name = '%s%d_rames.csv'%('../../outputs/irecommender/', ratio)
-        fail_file_name = '%s%d_fails.csv'%('../../outputs/irecommender/', ratio)
-        rmaes.append(wh.load_2ddata_from_file(rmae_file_name))
-        fails.append(wh.load_2ddata_from_file(fail_file_name))
 
-    return np.array(rmaes), np.array(fails)
+    for ratio in range(7):
+        file_name = '%s%d_result.json'%('../../outputs/irecommender/', ratio + 1)
+
+        with open(file_name, 'r') as f:
+            content = json.load(f)
+            average_rames.append(content['rmae_of_average'])
+            rmaes.append(content['rmaes'])
+            fails.append(content['fails'])
+
+    return np.array(rmaes), np.array(fails), np.array(average_rames)
 
 def draw_lines():
-    rmaes, _ = load_result()
-    markers = ['.', 'o', '*', '1']
-    num_options = [2, 4, 6, 8, 10]
+    rmaes, _, _ = load_result()
+    markers = ['.', 'o', '*', '1', '1', '2', '3']
+    num_options = [4, 6, 8, 10]
 
     labels = []
     for i in num_options:
         for j in num_options:
-            labels.append('%d * %d'%(i, j))
+            labels.append('%d*%d'%(i, j))
 
 
-    for i in range(4):
+    for i in range(7):
         plt.plot(labels, rmaes[i].T.flatten(), marker=markers[i])
 
     plt.show()
 
-draw_lines()
+def compare_fails(width = 5):
+    _, fails, _ = load_result()
+    num_options = [4, 6, 8, 10]
+
+    labels = []
+    for i in num_options:
+        for j in num_options:
+            labels.append('%d*%d' % (i, j))
+
+    x = np.array(range(16)) + 1
+    for i in range(7):
+        plt.bar(x + width * i, fails[i].T.flatten(), label = 'ratio=%.1f'%((i + 1) * 0.1))
+    plt.legend()
+
+    plt.show()
+
+
+# draw_lines()
+compare_fails()
